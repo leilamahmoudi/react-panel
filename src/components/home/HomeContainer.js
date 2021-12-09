925d65f171ed134e5b09f0d01dc5b6646b161876
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-12-09 22:23:57 +0100

    Add axios

--- src/components/home/HomeContainer.js
+++ src/components/home/HomeContainer.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "./homeContainer.scss";
+const baseUrl = "https://d19d5.mocklab.io";
 
 const HomeContainer = () => {
+  useEffect(async () => {
+    const contact = await axios({
+      method: "get",
+      url: `${baseUrl}/v1/contacts`,
+    });
+    console.log(contact);
+  }, []);
   return (
     <div className="Home">
       <div className="container">HELLO Home</div>
